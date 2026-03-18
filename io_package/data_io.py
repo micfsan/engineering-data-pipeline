d6dc8cae734e01825aa7a22110d21d5199bab43a
Michele Sandes <michele.fs86@gmail.com>
2026-03-18 17:29:53 -0300

    Ajuste datasets

--- io_package/data_io.py
+++ io_package/data_io.py
@@ -6,10 +6,12 @@ class DataIO:
         self.config = config
 
     def ler_pedidos(self):
-        return self.spark.read.csv(self.config.pedidos_path, schema=pedidos_schema, header=True)
+        # O Spark varre a pasta e lê todos os CSVs de uma vez
+        return self.spark.read.json("data/pedidos/", schema=pedidos_schema)
 
     def ler_pagamentos(self):
-        return self.spark.read.csv(self.config.pagamentos_path, schema=pagamentos_schema, header=True)
+        # O Spark varre a pasta e lê todos os JSONs de uma vez
+        return self.spark.read.json("data/pagamentos/", schema=pagamentos_schema)
 
     def gravar_resultado(self, df):
         df.write.mode("overwrite").parquet(self.config.output_path)
